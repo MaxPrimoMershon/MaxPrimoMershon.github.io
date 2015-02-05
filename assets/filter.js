21be176147305dfe7adcc85cf655b8f426bc494d
Max Primo Mershon <mershon.max16@gmail.com>
2015-02-04 22:20:08 -0700

    Update filter.js

--- assets/filter.js
+++ assets/filter.js
@@ -1,34 +1,43 @@
-			$(document).ready(function() {
-				
-				//Filter
-				$('ul.nav-pills a').click(function() {
-					$('ul.nav-pills .active').removeClass('active');
-					$(this).parent().addClass('active');
+$(document).ready(function() {
+	
+	//Filter
+	$('ul.nav-pills a').click(function() {
+		$('ul.nav-pills .active').removeClass('active');
+		$(this).parent().addClass('active');
 
-					var filterVal = $(this).text().toLowerCase().replace(',','').replace(' ','-').replace(',','').replace(' ','-').replace(',','').replace(' ','-');
+		var filterVal = $(this).text().toLowerCase().replace(',','').replace(' ','-').replace(',','').replace(' ','-').replace(',','').replace(' ','-');
 
-					if(filterVal == 'all') {
-						$('div#portfolio div.hidden').fadeIn('slow').removeClass('hidden');
-					} else {
-						$('div#portfolio div.item').each(function() {
-							if(!$(this).hasClass(filterVal)) {
-								$(this).fadeOut('normal').addClass('hidden');
-							} else {
-								$(this).fadeIn('slow').removeClass('hidden');
-							}
-						});
-					}
-
-					return false;
-				});
-
-				//Search
-				$("#search-criteria").on("keyup", function() {
-					var g = $(this).val().toLowerCase();
-					$(".item h3").each(function() {
-						var s = $(this).text().toLowerCase();
-						$(this).closest('.item')[ s.indexOf(g) !== -1 ? 'show' : 'hide' ]();
-					});
-				});
-
+		if(filterVal == 'all') {
+			$('div#portfolio div.hidden').fadeIn('slow').removeClass('hidden');
+		} else {
+			$('div#portfolio div.item').each(function() {
+				if(!$(this).hasClass(filterVal)) {
+					$(this).fadeOut('normal').addClass('hidden');
+				} else {
+					$(this).fadeIn('slow').removeClass('hidden');
+				}
 			});
+		}
+
+		return false;
+	});
+
+	//Search
+	$("#search-criteria").on("keyup", function() {
+		var g = $(this).val().toLowerCase();
+		$(".item h3").each(function() {
+			var s = $(this).text().toLowerCase();
+			$(this).closest('.item')[ s.indexOf(g) !== -1 ? 'show' : 'hide' ]();
+		});
+	});
+
+});
+
+function validateSupportForm ( form ) {
+	if(form.email.value == '') {
+	  alert('Please enter your email address');
+	  form.email.focus();
+	  return false;
+	}
+	return true;
+}
